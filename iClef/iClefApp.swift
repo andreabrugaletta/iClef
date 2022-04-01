0ebaf40cd4615e050e287dd7777e9a9d4fa142e7
Andrea Brugaletta <andreabrugaletta@yahoo.it>
2022-04-01 12:01:39 +0200

    Initial Commit

---
+++ iClef/iClefApp.swift
@@ -0,0 +1,17 @@
+//
+//  iClefApp.swift
+//  iClef
+//
+//  Created by Andrea Brugaletta on 01/04/22.
+//
+
+import SwiftUI
+
+@main
+struct iClefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
